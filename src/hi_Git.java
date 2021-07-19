62f4b36d88e6a476dd173673d547aa7bcccfd110
Marina Vel <marinavelitskaia@gmail.com>
2021-07-19 16:43:42 -0400

    second commit

--- src/hi_Git.java
+++ src/hi_Git.java
@@ -1,5 +1,7 @@
 public class hi_Git {
     public static void main(String[] args) {
         System.out.println("Hi git");
+        //we  were shared project
+        //create remote repository under the
     }
 }
---
+++ src/third_time.java
@@ -0,0 +1,5 @@
+public class third_time {
+    public static void main(String[] args) {
+        //create new and see on gitHUb
+    }
+}
